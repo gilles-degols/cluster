cac529d8201da8d8b2e8540bbe0e1a9cd44a81f7
gilles-degols <gilles@degols.net>
2018-10-22 14:03:54 +0200

    Move Communication class to cluster library

---
+++ src/main/scala/net/degols/filesgate/libs/cluster/messages/Communication.scala
@@ -0,0 +1,101 @@
+package net.degols.filesgate.libs.cluster.messages
+
+import akka.actor.ActorRef
+import akka.pattern.ask
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, TimeoutException}
+import scala.util.{Failure, Random, Try}
+
+case class RemoteReply(content: Any)
+
+object Communication {
+  private val logger: Logger = LoggerFactory.getLogger(getClass)
+
+  private var _clusterTopology: Option[ClusterTopology] = None
+
+  /**
+    * Number of retries to execute if a message could not be sent
+    */
+  private var _maxRetries: Int = 10
+
+  def setMaxRetries(value: Int): Unit = if(value > 0) _maxRetries = value else throw new Exception("Value out of range")
+
+  def setClusterTopology(clusterTopology: ClusterTopology): Unit = _clusterTopology = Option(clusterTopology)
+
+  def sendWithReply(sender: ActorRef, workerTypeId: String, message: Any, timeoutSecond: Int = 10): Try[RemoteReply] = Try {
+    var attempt = 0
+    var result: Try[RemoteReply] = Failure(new Exception("Method not called"))
+    while(attempt < _maxRetries) {
+      val actorRefs: List[ActorRef] = _clusterTopology match {
+        case None =>
+          logger.error("ClusterTopology not yet available.")
+          List.empty[ActorRef]
+        case Some(topology) =>
+          topology.getWorkerActors(workerTypeId).filter(_.isRunning).map(_.workerActorRef)
+      }
+
+      if(actorRefs.nonEmpty) {
+        // Simply take one at random. If we want to do smarter load balancing, you need to handle it yourselves
+        val actorRef = Random.shuffle(actorRefs).head
+        result = internalSendWithReply(sender, actorRef, message, timeoutSecond)
+        if(result.isSuccess) {
+          return result
+        }
+      }
+
+      attempt += 1
+
+      if(attempt < _maxRetries) {
+        Thread.sleep(500L)
+      }
+    }
+
+    result.get
+
+
+  }
+
+  def sendWithReply(sender: ActorRef, actorRef: ActorRef, message: Any, timeoutSecond: Int = 10): Try[RemoteReply] = Try{
+    var attempt = 0
+    var result: Try[RemoteReply] = Failure(new Exception("Method not called"))
+    while(attempt < _maxRetries) {
+      result = internalSendWithReply(sender, actorRef, message, timeoutSecond)
+      if(result.isSuccess) {
+        return result
+      }
+      attempt += 1
+
+      if(attempt < _maxRetries) {
+        Thread.sleep(500L)
+      }
+    }
+
+    result.get
+  }
+
+  def sendWithoutReply(sender: ActorRef, actorRef: ActorRef, message: Any): Try[Unit] = Try{
+    actorRef.tell(message, sender)
+  }
+
+  private def internalSendWithReply(sender: ActorRef, actorRef: ActorRef, message: Any, timeoutSecond: Int = 10): Try[RemoteReply] = Try{
+    try{
+      Await.result(actorRef.ask(message, sender), timeoutSecond second) match {
+        case x: Throwable =>
+          logger.error(s"Got a generic Throwable exception (not expected) while sending a message to $actorRef: $x")
+          throw x
+        case x =>
+          logger.debug(s"Received result from $actorRef : $x")
+          RemoteReply(content=x)
+      }
+    } catch {
+      case x: TimeoutException =>
+        logger.warn(s"Got TimeoutException while trying to send a message to $actorRef.")
+        throw x
+      case x: Throwable =>
+        logger.error(s"Got a generic Throwable exception (not expected) while sending a message to $actorRef.")
+        throw x
+    }
+  }
+}
